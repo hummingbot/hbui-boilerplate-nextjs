6e75ae879b6dd2204d8e8263cee1f1a9ecc781af
Antonio Brandao <antoniobrandaodesign@gmail.com>
2023-02-07 16:09:44 +0000

    Remove local font import since its now back inside npm module

--- system/layout.js
+++ system/layout.js
@@ -1,9 +1,5 @@
 import tw from 'twin.macro'
 
-import '@hummingbot/hbui/assets/fonts/inter/inter.css'
-import '@hummingbot/hbui/assets/fonts/satoshi/satoshi.css'
-import '@hummingbot/hbui/assets/fonts/sfmono/sfmono.css'
-
 const DefaultLayout = ({ children }) => (
   <div tw='bg-body' >
     {children}
